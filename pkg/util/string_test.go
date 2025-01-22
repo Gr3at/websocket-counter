c88362bc0ce041d7bda1edb96cb99c9d95a3fd40
Gr3at <33185243+Gr3at@users.noreply.github.com>
2025-01-22 17:48:04 +0200

    update RandString to return hex string. bench test. make command for bench test

--- pkg/util/string_test.go
+++ pkg/util/string_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-func TestRandString(t *testing.T) {
-	const allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+func TestRandHexString(t *testing.T) {
+	const allowedChars = "0123456789abcdef"
 
 	testCases := []int{0, 1, 5, 10, 50, 100}
 
@@ -30,5 +30,16 @@ func TestRandString(t *testing.T) {
 				t.Fatalf("Two random strings are identical: %s and %s. Is it truly random?", randomStr, randomStr2)
 			}
 		})
+	}
+}
+
+func BenchmarkRandHexString(b *testing.B) {
+
+	testCases := []int{0, 1, 5, 10, 41, 50, 100}
+
+	for _, testLength := range testCases {
+		for i := 0; i < b.N; i++ {
+			RandString(testLength)
+		}
 	}
 }
